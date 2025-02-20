5f4c357875013e5598386850261c2cf11e18ec8a
Asmaa Khaled <114343478+Asmaaakhaled@users.noreply.github.com>
2025-02-20 17:24:47 +0200

    Add files via upload

---
+++ day1/form/index.php
@@ -0,0 +1,29 @@
+<?php
+$firstname = $_POST['first_name'];
+echo "first_name: " . $firstname;
+echo "<br><br>";
+$lastname = $_POST['last_name'];
+echo "last_name: " . $lastname;
+echo "<br><br>";
+$address = $_POST['address'];
+echo "address: " . $address;
+echo "<br><br>";
+$country = $_POST['country'];
+echo "country: " . $country;
+echo "<br><br>";
+$gender = $_POST['gender'];
+echo "gender: " . $gender;
+echo "<br><br>";
+$skill = $_POST['skills'];
+echo "skills: " . $skill;
+echo "<br><br>";
+$username = $_POST['username'];
+echo "username: " . $username;
+echo "<br><br>";
+$password = $_POST['password'];
+echo "password: " . $password;
+echo "<br><br>";
+$department = $_POST['department'];
+echo "department: " . $department;
+echo "<br><br>";
+?>
